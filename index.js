a783b7227f04d26f598ec72c620d1a87f8c9d710
Ankeshkewat <ankeshkewat966@gmail.com>
2023-01-31 01:11:21 +0530

    github calander added

---
+++ index.js
@@ -0,0 +1,14 @@
+//------------ RESUME BUTTON--------------
+
+let ResumeBtn=document.getElementById("homeBtndiv")
+ResumeBtn.onclick=()=>{
+  window.open("./Ankesh_Kewat_Resume.pdf", "_blank")
+}
+
+
+let navResume=document.getElementById("navResume")
+navResume.onclick=()=>{
+  window.open("./Ankesh_Kewat_Resume.pdf", "_blank")
+}
+
+GitHubCalendar(".calendar", "Ankeshkewat", { responsive: true });
